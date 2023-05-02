b0a90a87f8c17fc8e104601f317b06f92da82385
AgustinG130 <111713095+AgustinG130@users.noreply.github.com>
2023-05-02 10:19:07 -0300

    Update ejercicio9diasdelasemana.py

--- ejercicio9diasdelasemana.py
+++ ejercicio9diasdelasemana.py
@@ -1,4 +1,5 @@
 dia = input("\n 1- Lunes \n 2- Martes \n 3- Miercoles \n 4- Jueves \n 5- Viernes \n 6- Sabado \n 7- Domingo \n Escriba su dia ")
+
 match dia:
     case "Lunes":
         print("Hoy comienza la semana. Animo!")
